0ac33405fa2eab7c28ba216b78064a99bd410ca7
Shawnzhu2051 <shawnzhu@ShawndeMacBook-Pro.local>
2017-04-06 00:22:13 +0800

    对于输入的一些修改

--- input_test.py
+++ input_test.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -7,6 +8,7 @@ import os
 import tensorflow.python.platform
 
 import tensorflow as tf
+import cv2
 
 #from tensorflow.models.image.cifar10 import cifar10_input
 #from tensorflow.python.util import compat
@@ -22,10 +24,8 @@ class inputtest(tf.test.TestCase):
         record = bytes(bytearray([label] + [red] * image_size + [green] * image_size + [blue] * image_size))
         expected = [[[red, green, blue]] * 32] * 32
         return record, expected
-    '''
 
-    def testSimple(self):
-        '''
+    def xxxxx():
         labels = [0, 1, 0]
         records = [self._record(labels[0], 1, 128, 255),
                    self._record(labels[1], 2, 0, 1),
@@ -38,6 +38,55 @@ class inputtest(tf.test.TestCase):
         '''
 
 
+    n = 1
+    table1 = []
+    table2 = []
+    table3 = []
+    table4 = []
+    table5 = []
+
+    def mybin(num):
+        bstr = bin(num)
+        bstr = '0' + bstr
+        return bstr.replace('0b', '')
+
+
+    def myhex(num):
+        hstr = hex(num).replace('0x', '')
+        if len(hstr) == 1:
+            hstr = '0' + hstr
+        return hstr
+
+
+    def processing(n):
+        sequence = str(n)
+        string = 'cat.' + sequence
+
+        image = cv2.imread('/Users/shawnzhu/Desktop/Project/Cat_and_Dog/train/' + string + '.jpg')#按照文件名打开文件
+        thirtytwo = cv2.resize(image, (32, 32), interpolation=cv2.INTER_AREA)#调整至32*32大小
+
+        records = []
+        label = [0,1]
+        records.append(mybin(label[0]))
+
+        for i in range(32):
+            for j in range(0, 32, 2):
+                temp1 = myhex(thirtytwo[i, j, 0])
+                temp2 = myhex(thirtytwo[i, j + 1, 0])
+                records.append(temp1 + temp2)
+        for i in range(32):
+            for j in range(0, 32, 2):
+                temp1 = myhex(thirtytwo[i, j, 1])
+                temp2 = myhex(thirtytwo[i, j + 1, 1])
+                records.append(temp1 + temp2)
+        for i in range(32):
+            for j in range(0, 32, 2):
+                temp1 = myhex(thirtytwo[i, j, 2])
+                temp2 = myhex(thirtytwo[i, j + 1, 2])
+                records.append(temp1 + temp2)
+        contents = b"".join([record for record in records])
+        with open('/Users/shawnzhu/Desktop/Project/Cat_and_Dog/train_data1.txt', 'wb') as filename:
+            filename.write(contents)
 
         with self.test_session() as sess:
             q = tf.FIFOQueue(99, [tf.string], shapes=())
